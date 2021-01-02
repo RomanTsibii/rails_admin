06896e4e638e7398aa4851f4aa57b85b5ef02a80
RomanTsibii <romanot70@gmail.com>
2021-01-02 15:46:42 +0200

    Edit credentials

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'pg', '~> 1.1'
 gem 'puma', '~> 5.0'
 gem 'rails', '~> 6.1.0'
 gem 'rails_admin', '~> 2.0', '>= 2.0.2'
+gem 'r_creds', '~> 1.0', '>= 1.0.2'
 gem 'sass-rails', '>= 6'
 gem 'turbolinks', '~> 5'
 gem 'webpacker', '~> 5.0'
@@ -31,7 +32,6 @@ group :development do
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem 'r_creds'
 gem 'redis'
 gem 'sidekiq'
 gem 'slim-rails'
--- config/initializers/mailjet.rb
+++ config/initializers/mailjet.rb
@@ -1,7 +1,7 @@
 Mailjet.configure do |config|
-  config.api_key = '03eb024cb53bd2d1d74483ce5e209fbc'
-  config.secret_key = '5e75ceb7f3a4a874e7f1dc66d015b911'
-  config.default_from = 'roman.ts@gmail.com'
+  config.api_key = RCreds.fetch(:mailjet, :api_key)
+  config.secret_key = RCreds.fetch(:mailjet, :secret_key)
+  config.default_from = RCreds.fetch(:mailjet, :default_from) # roman.ts@coaxsoft.com
   # Mailjet API v3.1 is at the moment limited to Send API.
   # We’ve not set the version to it directly since there is no other endpoint in that version.
   # We recommend you create a dedicated instance of the wrapper set with it to send your emails.
